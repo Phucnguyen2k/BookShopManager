b41e36eb7486973555c6724965a254eacc7754d2
Kasumi <0947466236p@gmail.com>
2023-10-03 21:34:25 +0700

    Add project files.

---
+++ Splash.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BookShopManager
+{
+    public partial class Splash : Form
+    {
+        //code move windown
+        private Point startPoint;
+        private bool isDragging = false;
+        private void lbTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                startPoint = e.Location;
+            }
+        }
+
+        private void lbTitle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point endPoint = PointToScreen(e.Location);
+                Location = new Point(endPoint.X - startPoint.X, endPoint.Y - startPoint.Y);
+            }
+        }
+        //State Window
+        private void lbTitle_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void btnMax_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+                this.WindowState = FormWindowState.Maximized;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnMini_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        public Splash()
+        {
+            InitializeComponent();
+        }
+    }
+}
